ddddaef7f9d236042cdea2bcd2fd2b4412b07497
Alex Zaenchkovskiy <alexey.zaenchkovskiy@aplana.com>
2017-08-18 11:28:05 +0300

    Modify SympleArithmetic.java

--- src/main/java/com/aplana/study/SimpleArithmetic.java
+++ src/main/java/com/aplana/study/SimpleArithmetic.java
@@ -9,7 +9,6 @@ public class SimpleArithmetic {
 
         int mark = 0;
         int result = Integer.parseInt(expression.substring(0, 1));
-//        int result = 0;
 
         for (int i = 1; i < expression.length(); i++){
             if (expression.substring(i, i+1).equals(Function.PLUS)){
@@ -20,15 +19,6 @@ public class SimpleArithmetic {
                 result -= Integer.parseInt(expression.substring(i+1, i+2));
             }
         }
-//        for (int i = 1; i < expression.length(); i++){
-//            if (expression.substring(i, i+1).equals(Function.PLUS)){
-//                result += Integer.parseInt(expression.substring(mark, i));
-//                mark = i + 1;
-//            }else if (expression.substring(i, i+1).equals(Function.MINUS)){
-//                result -= Integer.parseInt(expression.substring(mark, i));
-//                mark = i + 1;
-//            }
-//        }
 
         return result;
     }
